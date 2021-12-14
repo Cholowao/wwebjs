cb577f8ce1a22cb030d2a713ab63219533a2ecfd
shop pc <cholowao@gmail.com>
2021-12-14 14:45:18 +0300

    Innitial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+const { MessageMedia } = require('whatsapp-web.js');
+
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+
+    const media = MessageMedia.fromFilePath('./file.png');
+        if(msg.body === '!media') {
+        client.sendMessage(msg.from, media);
+    }
+    });
+
+client.initialize();
